e192876f8a2e00cf93b85b4b994831fdcb87b865
Harshit <harshit0300@gmail.com>
2021-08-12 17:31:52 +0530

    algo1

---
+++ gfg/SelectionSort.java
@@ -0,0 +1,65 @@
+// { Driver Code Starts
+#include <stdio.h>
+ 
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+int select(int arr[], int i);
+void selectionSort(int arr[], int n);
+ 
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+ 
+// Driver program to test above functions
+int main()
+{
+    int arr[1000],n,T,i;
+  
+    scanf("%d",&T);
+    
+    while(T--){
+        
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+      
+    selectionSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+int select(int arr[], int i)
+{
+    // code here such that selecionSort() sorts arr[]
+}
+
+
+void selectionSort(int arr[], int n)
+{
+  //code here
+  for(int i=0;i<n-1;i++)
+  {
+      int minid = i;
+      for(int j=i+1;j<n;j++)
+      {
+          if(arr[j]<arr[minid])
+            minid=j;
+      }
+      int t = arr[minid];
+      arr[minid]=arr[i];
+      arr[i]=t;
+  }
+}
